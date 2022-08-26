23c0b5c3b6bee9f3970682224f6d2e3758ee4a8b
joão.frança <joaofrancaf@hotmail.com>
2022-08-26 11:32:33 -0300

    trying outlet component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Pricing from "./page/pricing/Pricing";
 import About from "./page/about/About";
+import Product from "./page/pricing/products/Product";
 
 export function RouteApp () {
     <Routes>
-        <Route path="pricing" element={<Pricing/>}/>
+        <Route path="pricing" element={<Pricing/>}>
+            <Route path="product" element={<Product/>} />
+        </Route>
         <Route path="about" element={<About/>}/>
     </Routes>
 }
--- src/page/pricing/Pricing.tsx
+++ src/page/pricing/Pricing.tsx
@@ -1,10 +1,19 @@
+import { Link, Outlet } from "react-router-dom"
 
 export function Pricing() {
 
   return (
-    <div>
-      <h1>pagina pricing</h1>
-    </div>
+    <>
+      <div>
+        <h1>pagina pricing</h1>
+      </div>
+
+      <div>
+        <Link to="product"/>
+      </div>
+
+      <Outlet/>
+    </>
   )
 }
 
